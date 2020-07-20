bb0f2ffdb024e29712d44452a59b0d0cbbc4dbd3
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-07-20 12:06:34 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,13 +9,17 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    @song.notes.build
+    @song.notes.build
+    @song.notes.build
   end
 
   def create
-    @song = Song.new(song_params)
+    artist = Artist.find_orcreate_by(name: song_params[:artist_name])
+    @song = artist.songs.build(song_params)
 
     if @song.save
-      redirect_to @song
+      redirect_to songs_path
     else
       render :new
     end
@@ -46,8 +50,9 @@ class SongsController < ApplicationController
 
   private
 
+  #these params were so hard - had to look this up too
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, genre_id, :note_contents => [])
   end
 end
 
